c6eaeb2799321966e07890c4dc8ca7253858ebae
Sachin Kamath <sskamath96@gmail.com>
2015-03-15 16:28:44 +0530

    Update new_encryption.cpp

--- new_encryption.cpp
+++ new_encryption.cpp
@@ -11,18 +11,15 @@ int main()
 	cout<<"\n\nEnter the string : ";
 	cin.getline(word,20);
 	cout<<"\n\nThe plain string is "<<word<<endl;
+	
+	
 	for(int i=0; i<strlen(word); i++)
 	for(int j=i; j<strlen(word); j++)
 	{
-        word[i]+=word[j];
+        	word[i]+=word[j];
 	}
 
 	cout<<"The encrypted word is "<<word;
-    //for(int k=0; k<strlen(word); k++)
-	//for(int l=k; l<strlen(word); l++)
-	//{
-
-	//}
-	//cout<<"The decrypted word is "<<word;
+   
 	return 0;
 }
